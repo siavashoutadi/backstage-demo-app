ac0dc6f2dc554048b034d1ea376e50053ad6902c
Siavash Outadi <siavash.outadi@gmail.com>
2024-01-09 21:36:46 +0100

    Initialize the spring boot app

---
+++ src/main/java/com/example/backstagedemo/BackstageDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.backstagedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackstageDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackstageDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/backstagedemo/BackstageDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backstagedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackstageDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
